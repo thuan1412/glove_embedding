9a100f45b3899b465eb6bb373999e1e3ea0904e7
thuandh <thuan14121999@gmail.com>
2020-01-14 11:54:12 +0700

    add .to(device) for tensor

--- train.py
+++ train.py
@@ -11,10 +11,11 @@ from glove_model import GloveModel
 EMBED_DIM = 300
 
 N_EPOCHS = 100
-BATCH_SIZE = 256
+BATCH_SIZE = 2048
 X_MAX = 100
 ALPHA = 0.75
 
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 def weight_func(x, x_max, alpha):
     wx = (x / x_max)**alpha
@@ -29,12 +30,14 @@ def wmse_loss(weights, inputs, targets):
 
 dataset = GloveDataset(open("./text8").read(), n_words=10000000)
 glove = GloveModel(dataset._vocab_len, EMBED_DIM)
+glove.to(device)
 
 
 n_batches = int(len(dataset._xij) / BATCH_SIZE)
 loss_values = list()
 
 optimizer = optim.Adagrad(glove.parameters(), lr=0.05)
+
 if __name__ == "__main__":
     for e in range(1, N_EPOCHS + 1):
         batch_i = 0
@@ -44,14 +47,14 @@ if __name__ == "__main__":
             batch_i += 1
             optimizer.zero_grad()
 
-            outputs = glove(i_idx, j_idx)
+            outputs = glove(i_idx.to(device), j_idx.to(device))
             weights_x = weight_func(x_ij, X_MAX, ALPHA)
             loss = wmse_loss(weights_x, outputs, torch.log(x_ij))
             loss.backward()
             optimizer.step()
 
             loss_values.append(loss.item())
-            if batch_i % 100 == 0:
+            if batch_i % 250 == 0:
                 print("Epoch: {}/{} \t Batch: {}/{} \t Loss: {}".format(
                     e, N_EPOCHS, batch_i, n_batches, np.mean(loss_values[-20:])))
 
--- visualize_vector.py
+++ visualize_vector.py
@@ -4,12 +4,14 @@ import torch.nn as nn
 import json
 
 from glove_model import GloveModel
-from train import EMBED_DIM, dataset
+from train import EMBED_DIM, dataset, device
 
 cos = nn.CosineSimilarity()
 
+print(device)
 glove_model = GloveModel(dataset._vocab_len, EMBED_DIM)
 glove_model.load_state_dict(torch.load("./text8.pt"))
+glove_model.to(device)
 glove_model.eval()
 
 embedding = glove_model.wi.weight.data.numpy() + glove_model.wj.weight.data.numpy()
@@ -20,14 +22,13 @@ mapping_dicts = json.loads(open("mapping.json").read())
 def words_similar(word1, word2):
     vector1 = embedding[mapping_dicts['word2id'][word1]]
     vector2 = embedding[mapping_dicts['word2id'][word2]]
-    a = cos(torch.Tensor([vector1]), torch.Tensor([vector2])).item()
+    a = cos(torch.Tensor([vector1]).to(device), torch.Tensor([vector2]).to(device)).item()
     return a
 
 def most_similar(word, n=10):
     vector = embedding[mapping_dicts['word2id'][word]]
-    similar_vector = [ cos(torch.Tensor([vector]), torch.Tensor([i])).item()
+    similar_vector = [ cos(torch.Tensor([vector]).to(device), torch.Tensor([i]).to(device)).item()
                         for i in embedding ]
     similar_dict = {word: vector for word, vector in zip(mapping_dicts['id2word'].values(), similar_vector)}
     similar_dict = sorted(similar_dict.items(), key= lambda item: item[1], reverse=True)
     return similar_dict[1:n+1]
-most_similar("tsunami")
